25ac7c3c2439f4cff62d657da79ee132ca2b4c09
Pinnosee <dothanhminhphu26@gmail.com>
2025-04-25 16:31:38 +0700

    new Frame and DAO

---
+++ Pharmacy_Management/src/GUI/Frame_HoaDon_TraThuoc.java
@@ -0,0 +1,42 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Frame_HoaDon_TraThuoc extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame_HoaDon_TraThuoc frame = new Frame_HoaDon_TraThuoc();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Frame_HoaDon_TraThuoc() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
